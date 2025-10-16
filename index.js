32df49afac97b720ec2474767c4335d0dbe34e3f
LukaSpelberg <luka.spelberg@hva.nl>
2025-10-16 18:10:50 +0200

    weer een poging tot een fix

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const limiter = rateLimit({
   windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS, 10), // 15 minutes
   max: parseInt(process.env.RATE_LIMIT_MAX, 10), // Limit each IP to 100 requests per windowMs
   message: { message: process.env.RATE_LIMIT_MESSAGE },
+  trustProxy: false,
 });
 // Apply the rate limiter to all requests
 app.use(limiter);
